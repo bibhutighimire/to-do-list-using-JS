15a653cc8409f81c567b70acb902f301d8f6de37
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-08 10:32:48 -0600

    added add new item js codes and workable delete btn

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,57 @@
-// <main> (the parent container of the images)
-const imageGallery = document.querySelector("main");
-// List of all images in main
-const images = imageGallery.querySelectorAll("img");
-// Search bar in the header
-const searchBar = document.querySelector("#search");
-// Clear button for the search bar
-const clearButton = document.querySelector("#clear");
-
-for (image of images)
 {
-    
-    // Create a new element;
-    let newDiv = document.createElement("div");
+const list=document.querySelector("section>ul");
+console.log(list);
+const item=document.querySelector(".item"); 
+const input=document.querySelector("input");     
+const a = document.querySelector("a"); 
+let itemArray = [];
 
-    // 1. Find the image's parent
-    // 2. Replace the image as a child of the parent with the div
-    // 3. Add the image as a child to the div
-
-    // Main > Img 
-    // Becomes
-    // Main > Div > Img
-
-    // 1.
-    let parentOfTestImage = image.parentNode;
-    // Nothing changes yet.
-
-    // 2.
-    parentOfTestImage.replaceChild(newDiv, image);
-    // Main > Div (no Img, got replaced with Div)
-
-    // 3.
-    newDiv.appendChild(image);
-    // Main > Div > Img
-
-    // Generate a paragraph to store the caption.
-    let caption = document.createElement("p");
-    
-
-    // Set the caption to the alt text of the image.
-    caption.innerHTML = `${image.getAttribute("alt")}<br>`;
-
-    // Get tags attribute (full string).
-    let tags = image.getAttribute("tags");
-    // Split the tags attribute on the space character, into a list of each tag.
-    let tagList = tags.split(" ");
-    // For each tag...
-    for (tag of tagList)
-    {
-        // Create a link.
-        let tagLink = document.createElement("a");
-        // Set the text of the link to our tag.
-        tagLink.innerHTML = tag;
-        // Set the reference of the link to the current page.
-        tagLink.setAttribute("href", "#");
-        // We add an event listenter to the click event.
-        tagLink.addEventListener("click", function(event) {
-            // We prevent the default behaviour from occuring (don't reload the page).
-            event.preventDefault();
-            // We set the value of the search bar to the tag value.
-            searchBar.setAttribute("value", tagLink.innerHTML);
-        });
-
-        // We add the tag to the paragraph (caption).
-        caption.appendChild(tagLink);
-    }
-
-    // Add the caption to the div.
-    newDiv.appendChild(caption);
-
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("Delete");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+a.addEventListener("click",()=>
+{
+
+    
+    let newListItem = document.createElement("li");
+    newListItem.innerText = input.value.trim();
+
+    itemArray.push(input.value.trim().toLowerCase());
+    list.appendChild(newListItem);
+    input.value="";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("Delete");
+    span.className = "close";
+    span.appendChild(txt);
+    newListItem.appendChild(span);
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
 
 
+});
 
-
-
-
+}
