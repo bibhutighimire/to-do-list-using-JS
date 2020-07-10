b090765acbf33b4371d51545a18719e59edbfe0d
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-10 13:55:43 -0600

    added back button which partially works but need to remove endtime and back button from active list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 {
+
     const list = document.querySelector("section>ul");
     const input = document.querySelector("input");
     const a = document.querySelector("a");
@@ -11,15 +12,15 @@
     var myNodelist = document.getElementsByTagName("li");
 
     var j;
-    const dateElement=document.getElementsByClassName("date");
-    
+    const dateElement = document.getElementsByClassName("date");
+
 
     for (j = 0; j < myNodelist.length; j++) {
         //for date
-        const today=new Date();
-            var span2 = document.createElement("SPAN2");
-            var txt2 = document.createTextNode(today.toLocaleTimeString());
-            span2.className = "date";
+        const today = new Date();
+        var span2 = document.createElement("SPAN2");
+        var txt2 = document.createTextNode(today.toLocaleTimeString());
+        span2.className = "date";
         //for complete button
         var span = document.createElement("SPAN");
         var txt = document.createTextNode("Complete");
@@ -30,7 +31,7 @@
         span1.className = "close";
 
     }
-   
+
 
     // Click on a delete button to hide the current list item
     var close = document.getElementsByClassName("close");
@@ -42,29 +43,53 @@
             div.style.display = "none";
         }
     }
-    const asidelist = document.querySelector("aside>ul");   
-    const asideulli = document.querySelector("aside>ul>li");   
+    const asidelist = document.querySelector("aside>ul");
+    const asideulli = document.querySelector("aside>ul>li");
     let asideArray = [];
-// Click on a complete button to hide the active to do to completed to do
+    // Click on a complete button to hide the active to do to completed to do
     var complete = document.getElementsByClassName("complete");
     var i;
     for (i = 0; i < complete.length; i++) {
-        complete[i].onclick = function () 
-        {
-            
+        complete[i].onclick = function () {
+            console.log("heeeee from complete");
+
             var div = this.parentElement;
-            div.style.display = "none";   
+            div.style.display = "none";
             let asidelist = document.createElement("li");
             asidelist.innerText = div.value;
-            asideArray.push(div.value);           
-            list.appendChild(asidelist);         
+            asideArray.push(div.value);
+            list.appendChild(asidelist);
         }
     }
-  
+
+    var back = document.getElementsByClassName("back");
+    var i;
+    for (i = 0; i < back.length; i++) {
+        back[i].onclick = function () {
+            console.log('hi');
+            var div = this.parentElement;
+            div.style.display = "none";
+            console.log(div);
+            document.querySelector("section>ul").appendChild(document.createElement("li"));
+            itemArray.push(div.value);
+            document.querySelector("section>ul>li").appendChild(div.value);
+
+
+            // var div = this.parentElement;
+            // // div.style.display = "none";   
+            //  let asidelist = document.createElement("li");
+            //  asidelist.innerText = div.value;
+            // itemArray.push(div.value);           
+            // list.appendChild(div);         
+        }
+    }
+
+
     a.addEventListener("click", () => {
         // Keep track of if the current submission has an error.
         let errors = false;
         // If the input is empty.
+
         if (input.value.trim() === "") {
             error.innerText = "Sorry, please ensure you enter an item before attempting to add it.";
             errors = true;
@@ -74,9 +99,8 @@
             error.innerText = "Sorry, please ensure that the item you have entered is not already on your list.";
             errors = true;
         }
-        if (!errors) 
-        { 
-          
+        if (!errors) {
+
             // Clear any errors from previous submissions if this one is valid.
             error.innerText = "";
 
@@ -86,12 +110,11 @@
             itemArray.push(input.value.trim().toLowerCase());
             // Using Date() method 
 
-            if (list.className === "empty")
-            {
+            if (list.className === "empty") {
                 // 3a. If it is the first item, remove the empty class from
                 // the list.
                 list.classList.remove("empty");
-            
+
                 // 3b. If it is the first item, remove the "No items yet!" item.
                 // If the list still has the empty class, it should only
                 // have the "no items" li.
@@ -101,26 +124,31 @@
             list.appendChild(newListItem);
             input.value = "";
 
-            const today=new Date();
+            const today = new Date();
             var span2 = document.createElement("SPAN2");
-            var txt2 = document.createTextNode("Start: "+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() +" , "+today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate());
+            var txt2 = document.createTextNode("Start: " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " , " + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
             span2.className = "date";
-            
 
 
-        //     var span2 = document.createElement("SPAN2");
-        // const today=new Date();
-        // //dateElement.innerHTML=today.toLocaleDateString();
-        // dateElement.innerHTML = today.toString();
-        // span2.className = "date";
+
+            //     var span2 = document.createElement("SPAN2");
+            // const today=new Date();
+            // //dateElement.innerHTML=today.toLocaleDateString();
+            // dateElement.innerHTML = today.toString();
+            // span2.className = "date";
 
             var span = document.createElement("SPAN");
             var txt = document.createTextNode("Complete");
             span.className = "complete";
-            
+
             var span1 = document.createElement("SPAN1");
             var txt1 = document.createTextNode("Delete");
             span1.className = "close";
+
+            var span5 = document.createElement("SPAN5");
+            var txt5 = document.createTextNode("Edit");
+            span5.className = "edit";
+
 
             span2.appendChild(txt2);
             newListItem.appendChild(span2);
@@ -132,48 +160,69 @@
             span1.appendChild(txt1);
             newListItem.appendChild(span1);
 
-            
+            span5.appendChild(txt5);
+            newListItem.appendChild(span5);
 
             for (i = 0; i < close.length; i++) {
 
                 close[i].onclick = function () {
+
                     var div = this.parentElement;
                     div.style.display = "none";
                 }
             }
-            let selected=document.querySelector("aside>ul>li");
 
-console.log(selected)
+            let selected = document.querySelector("aside>ul>li");
             for (i = 0; i < complete.length; i++) {
-                complete[i].onclick = function () 
-                {                  
-                    var sectionorigin = this.parentElement;                    
-                      
+                complete[i].onclick = function () {
+                    var sectionorigin = this.parentElement;
                     let asidelistItem = document.createElement("li");
-                    
-             //appending end date        
-            var span3 = document.createElement("SPAN3");
-            const today=new Date();
-            var txt3 = document.createTextNode("End: "+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() +" , "+today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate());
-            console.log(txt3.parentElement);
-            span3.className = "date";
-            span3.appendChild(txt3);
-            sectionorigin.appendChild(span3);
 
-            var span4 = document.createElement("SPAN4");
-            var txt4 = document.createTextNode("Edit");
-            span4.className = "edit";
-            span4.appendChild(txt4);
-            sectionorigin.appendChild(span4);
+                    //appending end date        
+                    var span3 = document.createElement("SPAN3");
+                    const today = new Date();
+                    var txt3 = document.createTextNode("End: " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + " , " + today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate());
+                    console.log(txt3.parentElement);
+                    span3.className = "date";
+                    span3.appendChild(txt3);
+                    sectionorigin.appendChild(span3);
 
-                    const completedtime=new Date();
-                    asidelistItem.innerText =document.createTextNode(/*completedtime.getHours() + ":" + completedtime.getMinutes() + ":" + completedtime.getSeconds() +","+completedtime.getFullYear()+'-'+(completedtime.getMonth()+1)+'-'+completedtime.getDate()) +"-"+*/  sectionorigin.children[1].remove());
+                    var span4 = document.createElement("SPAN4");
+                    var txt4 = document.createTextNode("Back");
+                    span4.className = "back";
+                    span4.appendChild(txt4);
+                    sectionorigin.appendChild(span4);
+                    asidelistItem.innerText = document.createTextNode(sectionorigin.children[1].remove());
+                    asidelistItem.innerText = document.createTextNode(sectionorigin.children[2].remove());
+                    asideArray.push(sectionorigin.value);
+                    asidelist.appendChild(sectionorigin);
 
-                    asideArray.push(sectionorigin.value);           
-                    asidelist.appendChild(sectionorigin);        
+
+                    for (i = 0; i < back.length; i++) {
+                        back[i].onclick = function () {
+                            console.log("back inside aside");
+                            let ck = document.createElement("li");
+                            var backli = this.parentElement;
+
+                            // ck.innerText =document.createTextNode(backli.children[2].remove());
+                            // ck.innerText =document.createTextNode(backli.children[3].remove()); 
+
+                            itemArray.push(backli.value);
+
+                            list.appendChild(backli);
+
+
+                            // var div = this.parentElement;
+                            // // div.style.display = "none";   
+                            //  let asidelist = document.createElement("li");
+                            //  asidelist.innerText = div.value;
+                            // itemArray.push(div.value);           
+                            // list.appendChild(div);         
+                        }
+                    }
                 }
-            }        
+            }
         }
     });
-    
+
 }
